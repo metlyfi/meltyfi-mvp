1d99e8b7f12c15751aa3a4838fa0ca93dfd6ff9c
Mikalai Grazzini Yaskevich <one.emper@gmail.com>
2024-11-29 19:28:52 +0100

    fix

--- packages/nextjs/components/AddLottery.tsx
+++ packages/nextjs/components/AddLottery.tsx
@@ -3,8 +3,8 @@ import React, { ReactNode, useState } from 'react';
 import CustomCard from './Card';
 import { AddressInput, EtherInput, InputBase, IntegerInput } from './scaffold-eth';
 import { useScaffoldWriteContract } from '~~/hooks/scaffold-eth';
-import { parseEther } from 'viem';
 import { notification } from '~~/utils/scaffold-eth';
+// import { useAccount, useWriteContract } from 'wagmi';
 
 const AddLottery: React.FC<any> = ({  }) => {
   const { writeContractAsync: writeYourContractAsync } = useScaffoldWriteContract("MeltyFiNFT");
@@ -15,20 +15,46 @@ const AddLottery: React.FC<any> = ({  }) => {
   const [wonkaBarPrice, setWonkaBarPrice] = useState('')
   const [wonkaBarsMaxSuply, setWonkaBarsMaxSuply] = useState('')
 
+  // const { writeContract } = useWriteContract()
 
-  console.log('duration', duration, priceContractAddress, priceTokenId, wonkaBarPrice, wonkaBarsMaxSuply)
 
-  
+
 
   const handleCreateLottery = async () => {
-
     if(!duration) return notification.error(<ErrorCode text='Duration is empty!' />);
+    if(!priceContractAddress) return notification.error(<ErrorCode text='Contract Address is empty!' />);
+    if(!priceTokenId) return notification.error(<ErrorCode text='NFT ID is empty!' />);
+    if(!wonkaBarPrice) return notification.error(<ErrorCode text='Ticker Price is empty!' />);
+    if(!wonkaBarsMaxSuply) return notification.error(<ErrorCode text='Tickers Qumtity is empty!' />);
 
+    const date = new Date(duration);
+    const timestamp = date.getTime() / 1000; 
 
     try {
+    //  await writeContract({ 
+    //     abi: [
+    //       {
+    //         "inputs": [
+    //           { "internalType": "address", "name": "to", "type": "address" },
+    //           { "internalType": "uint256", "name": "tokenId", "type": "uint256" }
+    //         ],
+    //         "name": "approve",
+    //         "outputs": [],
+    //         "stateMutability": "nonpayable",
+    //         "type": "function"
+    //       }
+    //     ],
+    //     address: priceContractAddress,
+    //     functionName: 'approve',
+    //     args: [
+    //       '0xFf7d147cdD63f1c5E9351d97883DB2eb75AA3B8B',
+    //       BigInt(priceTokenId),
+    //     ],
+    //  })
+
       await writeYourContractAsync({
         functionName: "createLottery",
-        args: [duration, priceContractAddress, priceTokenId, wonkaBarPrice, wonkaBarsMaxSuply],
+        args: [BigInt(timestamp), priceContractAddress, BigInt(priceTokenId), BigInt(wonkaBarPrice),BigInt(wonkaBarsMaxSuply)],
       });
     } catch (e) {
       console.error("Error setting greeting:", e);
@@ -91,10 +117,8 @@ export default AddLottery;
 
 const ErrorCode = ({ text }: any) => {
   return (
-    <>
       <p className="font-bold mt-0 mb-1">
         <code className="italic bg-base-300 text-base font-bold"></code>{text}
       </p>
-    </>,
   )
 }
